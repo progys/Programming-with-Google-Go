c7cb800c166724ca7b041c1ffc4e5c06393d72f3
progys <progys@gmail.com>
2020-09-12 01:31:20 +0300

    read directly into int variable to avoid rounding

--- src/week2/truncate/truncate.go
+++ src/week2/truncate/truncate.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 /*
-* Teaches how to read from standard input, how to do simple type conversions and use pointers.
+ * Teaches how to read from standard input, how to do simple type conversions and use pointers.
  */
 func main() {
-	x := new(float64)
-	fmt.Println("Type in float number, I will convert it to int: ")
+	x := new(int64)
+	fmt.Println("Type in float number, I print it as int: ")
 	fmt.Scan(x)
-	fmt.Println("Your float converted to int is: ", int64(*x))
+	fmt.Println("Your float converted to int is: ", *x)
 }
