7dba1000ebe31225250443181053f168d84f2ec0
progys <progys@gmail.com>
2020-09-11 23:09:28 +0300

    add note what it is about

--- src/week1/hello-world/main.go
+++ src/week1/hello-world/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/**
+* Teaches how to use imports, main package and main function. How to compile code and run it.
+* Compile and execute by running "go run main.go".
+ */
 func main() {
 	fmt.Println("Hello, world!")
 }
