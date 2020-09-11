af2c0bb26d7a84c7b1b98bcefd8631f37665050a
progys <progys@gmail.com>
2020-09-11 23:08:14 +0300

    add notes what it is about

--- src/week2/truncate/truncate.go
+++ src/week2/truncate/truncate.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+/*
+* Teaches how to read from standard input, how do simple type conversions and use pointers.
+ */
 func main() {
 	x := new(float64)
 	fmt.Println("Type in float number, I will convert it to int: ")
