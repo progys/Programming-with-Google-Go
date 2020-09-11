b8948a70481ca5bb193190bd80f82dc4e4599c56
progys <progys@gmail.com>
2020-09-12 01:33:50 +0300

    add assignment description as comment

--- src/week2/truncate/truncate.go
+++ src/week2/truncate/truncate.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 /*
- * Teaches how to read from standard input, how to do simple type conversions and use pointers.
+ * Write a program which prompts the user to enter a floating point number and prints the integer
+ * which is a truncated version of the floating point number that was entered.
+ * Truncation is the process of removing the digits to the right of the decimal place.
+ *
+ * Teaches how to read from standard input and use pointers.
  */
 func main() {
 	x := new(int64)
