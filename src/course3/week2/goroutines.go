ab0bcd747605a1991c83a205729e2f27bbf9ecbc
progys <progys@gmail.com>
2020-09-27 09:43:53 +0300

    race-condition guaranteed

--- src/course3/week2/goroutines.go
+++ src/course3/week2/goroutines.go
@@ -7,38 +7,45 @@ import (
 
 /**
 * The goal of this activity is to explore race conditions by creating and running two simultaneous goroutines.
+*
+* Normally if it would be non-concurent program it would print sequentially incremented value on each iteration, but
+* now results are non deterministic.
+*
+* It uses two go routines. Execution order of those two used go routines is non deterministic and they both depend on the same shared variable
+* which is being read and altered and printed by those both routines at different points in time.
+* As those two go routines execute completely independently from each other and
+* program result depends on the execution order which is now non deterministic - this creates race condition or in other words non-deterministic result of the program itself.
  */
 
-//Shared is a shard structure between two routines
+//Shared is a shared structure between two routines
 type Shared struct {
 	value int
 }
 
 func incrementAndPrint(counter *Shared, name string) {
-	counter.value = counter.value + 1 //this value can be altered by other routine at any time
-	time.Sleep(1 * time.Second)       //this guarantees that race condition occurs
-	fmt.Println("Routine:", name, "value: ", counter.value)
+	for i := 0; i < 5; i++ {
+		counter.value = counter.value + 1 //this value can be altered by other routine at any time
+		time.Sleep(1 * time.Millisecond)  //this make sure race-condition occurs, as routine 1 and routine 2 reads the same value before altering
+		fmt.Println("Routine:", name, "value:", counter.value)
+	}
 }
 
-/*
-* Normally if it would be non-concurent program it would print icremented value on each iteration,
-* now it prints the same value in each concurent iteration.
-*
-* It uses two go routines. Execution order of those two used go routines is non deterministic and they both depend on the same shared variable
-* which is being read and altered and printed by those both routines at different points in time.
-* As those two go routines execute completely independently from each other and
-* program result depends on the execution order which is non deterministic - this creates race condition or in other words non-deterministic result of the program itself.
- */
 func main() {
+	for {
+		fmt.Println()
+		fmt.Println("I should print values from 1 to 10, but I do not!")
+		fmt.Println("Press ENTER to run with race condition, repeat to observe different results each time>")
+		fmt.Scanln()
+
+		runConcurently()
+	}
+}
+
+func runConcurently() {
 	sharedCounter := Shared{0}
 
-	fmt.Println()
-	fmt.Println("I should print values from 1 to 20. But I do not!")
-	fmt.Println()
-	for i := 0; i < 10; {
-		go incrementAndPrint(&sharedCounter, "r1")
-		go incrementAndPrint(&sharedCounter, "r2")
-		i++
-		time.Sleep(500 * time.Millisecond)
-	}
+	go incrementAndPrint(&sharedCounter, "1")
+	go incrementAndPrint(&sharedCounter, "2")
+	time.Sleep(500 * time.Millisecond)
+
 }
