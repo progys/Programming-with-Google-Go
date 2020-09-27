188d94de83cb796d5a42c1704a4aa6328ee9c9ed
progys <progys@gmail.com>
2020-09-27 09:06:22 +0300

    race condition

--- src/course3/week2/goroutines.go
+++ src/course3/week2/goroutines.go
@@ -11,26 +11,34 @@ import (
 
 //Shared is a shard structure between two routines
 type Shared struct {
-	doubleSum int
+	value int
 }
 
-func doubleAndPrint(x *Shared, name string) {
-	x.doubleSum = x.doubleSum + x.doubleSum //race condition here, because this value can be altered by other routine at any time
-	fmt.Println("Routine:", name, "sum: ", x.doubleSum)
+func incrementAndPrint(counter *Shared, name string) {
+	counter.value = counter.value + 1 //this value can be altered by other routine at any time
+	time.Sleep(1 * time.Second)       //this guarantees that race condition occurs
+	fmt.Println("Routine:", name, "value: ", counter.value)
 }
 
 /*
-* Normally if it would non-concurent program it would print doubling values on each iteration,
-* now it sometimes prints the same value in each concurent iteration or prints the smaller value after larger one.
-* Because execution order is not deterministic and this program depends on same shared value "sharedSum" which is being altered in non-deterministic way.
+* Normally if it would be non-concurent program it would print icremented value on each iteration,
+* now it prints the same value in each concurent iteration.
+*
+* It uses two go routines. Execution order of those two used go routines is non deterministic and they both depend on the same shared variable
+* which is being read and altered and printed by those both routines at different points in time.
+* As those two go routines execute completely independently from each other and
+* program result depends on the execution order which is non deterministic - this creates race condition or in other words non-deterministic result of the program itself.
  */
 func main() {
-	sharedSum := Shared{1}
+	sharedCounter := Shared{0}
 
-	for i := 0; i < 6; {
-		go doubleAndPrint(&sharedSum, "r1")
-		go doubleAndPrint(&sharedSum, "r2")
-		time.Sleep(1 * time.Second)
+	fmt.Println()
+	fmt.Println("I should print values from 1 to 20. But I do not!")
+	fmt.Println()
+	for i := 0; i < 10; {
+		go incrementAndPrint(&sharedCounter, "r1")
+		go incrementAndPrint(&sharedCounter, "r2")
 		i++
+		time.Sleep(500 * time.Millisecond)
 	}
 }
